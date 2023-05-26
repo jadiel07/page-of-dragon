f32fe2714576f6d1827b50e6478e3d769557743b
jadiel07 <jadiel174@hotmail.com>
2023-05-26 17:48:41 -0300

    adicionamento de novas funções
    nesta atualização trouxe algumas das novas funções da pagina, adicionei a transição de tema ao clicar no yin-yang da nav bar, além de adicionar um audio assim que o  tema é clicado, na próxima atualização pretendo mudar as imagens quando o yin-yang for ativado

--- script.js
+++ script.js
@@ -5,3 +5,22 @@ function openMenu() {
     burgerItens.style.display = "block";
   }
 }
+
+const yinYang = document.getElementById("yinYang");
+var body = document.getElementById("bodyPage");
+var corAlterada = false;
+var audio = document.getElementById("audio");
+
+yinYang.addEventListener("click", function () {
+  if (corAlterada) {
+    body.style.backgroundColor = "";
+    body.style.color = "";
+    corAlterada = false;
+  } else {
+    body.classList.add("transition");
+    body.style.backgroundColor = "black";
+    body.style.color = "white";
+    corAlterada = true;
+  }
+  audio.play();
+});
