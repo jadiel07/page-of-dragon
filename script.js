1e10fc4fef7098e5eef5eacad4fa7cab28e9509d
jadiel07 <jadiel174@hotmail.com>
2023-05-27 22:13:44 -0300

    atualização das imagens
    nesta atualização eu adicionei a imagem do tema escuro e removi os antigos cards que tinham em baixo, além disso também adicionei o hover ao passar pelos itens da nav-bar

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//menu hamburger
 function openMenu() {
   if (burgerItens.style.display == "block") {
     burgerItens.style.display = "none";
@@ -9,9 +10,21 @@ function openMenu() {
 const yinYang = document.getElementById("yinYang");
 var body = document.getElementById("bodyPage");
 var corAlterada = false;
+
+var imgDragon1 = document.getElementById("dragonWhite");
+var imgDragon2 = document.getElementById("dragonBlack");
+var imagensTrocadas = false;
+
+var conteudoCard = true;
+var card1 = document.getElementById("card1");
+var card2 = document.getElementById("card2");
+var card3 = document.getElementById("card3");
+var card0 = document.getElementById("card0");
+
 var audio = document.getElementById("audio");
 
 yinYang.addEventListener("click", function () {
+  //alteração na cor tema do site. alterning theme web site
   if (corAlterada) {
     body.style.backgroundColor = "";
     body.style.color = "";
@@ -22,5 +35,16 @@ yinYang.addEventListener("click", function () {
     body.style.color = "white";
     corAlterada = true;
   }
+
+  if (imagensTrocadas) {
+    imgDragon1.style.display = "block";
+    imgDragon2.style.display = "none";
+    imagensTrocadas = false;
+  } else {
+    imgDragon1.style.display = "none";
+    imgDragon2.style.display = "block";
+    imagensTrocadas = true;
+  }
+  //audio adicionado ao clicar no yin-yang
   audio.play();
 });
